3d9ddb18c72081325d2a2b3d3afc68e65c178b4a
ericcfrancis <ericcire49@gmail.com>
2025-01-05 20:20:28 +0800

    update routes for auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+// user authentication module
+Route::get('/register', function(){
+    return view('auth.register');
+})->name('register.form');
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::get('/dashboard', function (){
+    return view('auth.dashboard');
+})->name('dashboard');
+
+Route::get('/login', function(){
+    return view('auth.login');
+})->name('login.form');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// Route::post('/logout'. [AuthController::class, 'logout'])->middleware('auth');
