00da0c83c40ddf8508fb5e0b3cbf2cc6b51f3706
ericcfrancis <ericcire49@gmail.com>
2025-01-06 05:29:41 +0800

    update navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>my-test-app</title>
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.scss') }}" > -->
+     @vite(['resources/sass/app.scss'])
 </head>
 <body>
     <header>
-        <nav>
-            <a href="{{route('home') }}">Hello</a>
-            <a href="{{route('register.form') }}">Register</a>
-            <a href="{{route('login.form') }}">Login</a>
+        <nav class="navbar navbar-expand-lg bg-info">
+            <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a href="{{route('home') }}" class="navbar-brand">Test</a>
+            <div class="collapse navbar-collapse" id="navBarToggler">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('register.form') }}">register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('login.form') }}">login</a>
+                    </li>
+                </ul>
+
+            </div>
+            </div>
         </nav>
     </header>
 
-    <main>
+    <main class="container">
         @yield('content')
     </main>
 
