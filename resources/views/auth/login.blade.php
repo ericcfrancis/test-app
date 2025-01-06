2d3c4acfe55c6bcf448536ad9589443455f34e17
ericcfrancis <ericcire49@gmail.com>
2025-01-06 20:40:52 +0800

    update login page styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>test login page</h1>
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <!-- email -->
-        <div class="div">
-            <label for="name">Email</label>
-            <input type="text" id="email" name="email" value="{{ old('email') }}" required>
-            @error('email')
-                <span style="color: red;">{{ $message }}</span>
-            @enderror
+<div class="row justify-content-center align-items-center">
+    <div class="col-5 my-3">
+        <div class="row text-center">
+            <h1>test login page</h1>
         </div>
-        <!-- password -->
-        <div class="div">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-            @error('password')
-                <span style="color: red;">{{ $message }}</span>
-            @enderror
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <!-- email -->
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
+                @error('email')
+                    <span style="color: red;">{{ $message }}</span>
+                @enderror
+            </div>
+            <!-- password -->
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+                @error('password')
+                    <span style="color: red;">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="row justify-content-center">
+                <button class="btn btn-warning w-100 px-3" type="submit">login</button>
+            </div>
+        </form>
+        <div class="row text-center mt-2">
+            <small>doesn't have an account? register <a href="{{ route('register.form') }}">here</a></small>
         </div>
-        <button type="submit">Login</button>
-    </form>
+    </div>
+    <hr>
+</div>    
 @endsection
